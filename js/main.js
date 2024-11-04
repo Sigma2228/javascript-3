dda3d07d20f1c07ac1d310273fa4b1411b248539
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2024-11-04 21:56:57 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const result = 5 + 5 + '5'
+console.log(result)
+console.log(typeof result)
+
+const email = 'sigmobebra228@gmail.com'
+if(email.includes('@')) {
+    console.log('містить знак @')
+}
+else {
+    console.log('не містить знак @')
+}
+
+const my = 'my'
+const name = 'name'
+const is = 'is'
+const fullName = console.log(`${my} ${name} ${is} Viktor`)
+
+
+const userName = 'Сергій'
+const payment = 500
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень»`)
+
+
